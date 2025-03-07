4315302a4cdcdf119e02452dc11d18380292aee7
Alonso Perez Vazqeuz <38843076+AlonsoPV@users.noreply.github.com>
2025-03-06 20:19:25 -0600

    Update productoIndividual.jsx

--- eCommerce/src/Components/ProductoIndividual/productoIndividual.jsx
+++ eCommerce/src/Components/ProductoIndividual/productoIndividual.jsx
@@ -10,9 +10,11 @@ const ProductoIndividual = () => {
         ? ( 
           <div className="tarjeta">
             <img className="tarjeta_imagen" src={selectedProduct.image} alt={selectedProduct.product_name} />
+            <h4 className="tarjeta_etiqueta">Ver más</h4>
             <h4 className="tarjeta_nombre">{selectedProduct.product_name}</h4>
             <h5 className="tarjeta_descripcion">{selectedProduct.description}</h5>
             <h6 className="tarjeta_precio">$ {selectedProduct.price} MXN</h6>
+            <button onClick={() => anadirCarrito(product)}>🛒 Agregar al carrito</button>
           </div>
           )
         : <h1> selecciona un producto...</h1>
