5ee45df0f0345710a1ac876a43a098b7dbb800d6
Alonso Perez Vazqeuz <38843076+AlonsoPV@users.noreply.github.com>
2025-03-06 19:49:05 -0600

    Update Footer.jsx

--- eCommerce/src/Components/Footer/Footer.jsx
+++ eCommerce/src/Components/Footer/Footer.jsx
@@ -1,14 +1,15 @@
+import './Footer.css'
 const Footer = () => {
     return (
       <footer className="footer">
-        <p>© {new Date().getFullYear()} Mi Tienda | Todos los derechos reservados</p>
+        <p>© {new Date().getFullYear()} eMotion Sites</p>
         <p>
-          Desarrollado por <a href="https://tusitio.com" target="_blank">Tu Nombre</a>
+          Desarrollado por <a href="www.emotionsites.com" target="_blank">eMotion Sites</a>
         </p>
-        <div className="footer__social">
-          <a href="https://facebook.com" target="_blank">📘</a>
-          <a href="https://twitter.com" target="_blank">🐦</a>
-          <a href="https://instagram.com" target="_blank">📷</a>
+        <div className="footer__redes">
+          <a href="#" target="_blank">F</a>
+          <a href="#" target="_blank">I</a>
+          <a href="#" target="_blank">LIn</a>
         </div>
       </footer>
     );
